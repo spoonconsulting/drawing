8f67ff7af476b7a98024422f6a8036d6653ed937
Luc Boissaye <luc@boissaye.fr>
2021-03-23 12:11:37 -0400

    Improve handles position and priorities

--- src/drawing_transform.js
+++ src/drawing_transform.js
@@ -97,7 +97,7 @@ class DrawingTransform {
 
     var handle = Utils.create_element(this.svg, 'path', {
       d: `M${x1[0]},${x1[1]} L${x2[0]},${x2[1]} L${x3[0]},${x3[1]} L${x4[0]},${x4[1]} L${x1[0]},${x1[1]}`,
-      style: 'stroke: #CCCCCC; stroke-linecap: square; stroke-width: 50;fill:#CCCCCC; opacity: 0.3;'
+      style: 'stroke: #CCCCCC; stroke-linecap: square; stroke-width: 30;fill:#CCCCCC; opacity: 0.3;'
     })
 
     this.svg.insertBefore(handle, this.element)
@@ -123,37 +123,13 @@ class DrawingTransform {
       r: this.size / 2,
       fill: 'white'
     })
-    Utils.create_element(handle, 'circle', {
-      r: this.size / 4,
-      fill: '#0070d2'
-    })
-    Utils.apply_matrix(handle, [[1, 0, x3[0]], [0, 1, x3[1]], [0, 0, 1]])
-    var rotateScaleHandle = new Handle(handle, {
-      transformations: 'SR',
-      pivot: handleCenter,
-      start: (e) => {
-        this.start(e)
-        this.removeHandleExcept(rotateScaleHandle)
-      },
-      move: (matrix) => {
-        Utils.apply_matrix(this.element, MatrixUtils.mult(matrix, new Referentiel(this.element).matrixTransform()))
-      },
-      end: () => { this.end() }
-    })
-    this.handles.push(rotateScaleHandle)
-
-    handle = Utils.create_element(this.svg, 'g', {})
-    Utils.create_element(handle, 'circle', {
-      r: this.size / 2,
-      fill: 'white'
-    })
     Utils.create_element(handle, 'path', {
       r: this.size / 2,
       d: 'M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z',
       fill: '#0070d2',
       transform: 'scale(' + this.size / 1024 + ') translate(-256 -256)'
     })
-    Utils.apply_matrix(handle, [[1, 0, x1[0]], [0, 1, x1[1]], [0, 0, 1]])
+    Utils.apply_matrix(handle, [[1, 0, x4[0]], [0, 1, x4[1]], [0, 0, 1]])
     var rotateHandle = new Handle(handle, {
       transformations: 'R',
       pivot: handleCenter,
@@ -191,6 +167,31 @@ class DrawingTransform {
       cancel: () => { this.cancel() }
     })
     this.handles.push(scaleHandle)
+
+    handle = Utils.create_element(this.svg, 'g', {})
+    Utils.create_element(handle, 'circle', {
+      r: this.size / 2,
+      fill: 'white'
+    })
+    Utils.create_element(handle, 'circle', {
+      r: this.size / 4,
+      fill: '#0070d2'
+    })
+    Utils.apply_matrix(handle, [[1, 0, x3[0]], [0, 1, x3[1]], [0, 0, 1]])
+    var rotateScaleHandle = new Handle(handle, {
+      transformations: 'SR',
+      pivot: handleCenter,
+      start: (e) => {
+        this.start(e)
+        this.removeHandleExcept(rotateScaleHandle)
+      },
+      move: (matrix) => {
+        Utils.apply_matrix(this.element, MatrixUtils.mult(matrix, new Referentiel(this.element).matrixTransform()))
+      },
+      end: () => { this.end() }
+    })
+    this.handles.push(rotateScaleHandle)
+
   }
 
   makeACopy () {
